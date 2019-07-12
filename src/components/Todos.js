692836ec4cdb9bf758c4094d98c1a3459690d54c
Joakim <jmalmberg.web@gmail.com>
2019-07-12 20:09:34 +0200

    routing and axios/json

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -10,8 +10,11 @@ class Todos extends Component {
   }
 }
 
+// PropTypes
 Todos.propTypes = {
-  todos: PropTypes.array.isRequired
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
 }
 
 export default Todos;
